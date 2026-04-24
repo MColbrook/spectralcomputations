c52ac46cd55b2ca84188c254a949f860cb1b3051
Matthew Colbrook <mjc249@cam.ac.uk>
2026-04-24 15:33:38 +0100

    Delete chapter_7/routines directory

--- chapter_7/routines/log capacity/bie/clog.m
+++
@@ -1,9 +0,0 @@
-function  y   =   clog (z)
-% clog.m
-% Nasser, September 5, 2019
-% This function compute the a continuous branch function of log(z)
-%
-%
-%%
-y     =     log(abs(z))+i.*carg(z);
-%%
